38c5bbb9a86897c451d42516980c3413f1c4cdfa
Marcin Baran <mrevib@gmail.com>
2018-02-26 13:58:17 +0100

    poprawa wyswietlania diety

--- templates/wyswietl_diete.php
+++ templates/wyswietl_diete.php
@@ -27,96 +27,170 @@
         </tr>
         </thead>
         <tbody>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Grzyby');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){?>
                 <tr>
                     <td>Grzyby</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Grzyby'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Mięso i wędliny');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Mięso i wędliny</td>
                     <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Mięso i wędliny'); echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php }  ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Nabiał');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Nabiał</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Nabiał'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Napoje');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Napoje</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Napoje'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Orzechy, nasiona i bakalie');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Orzechy, nasiona i bakalie</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Orzechy, nasiona i bakalie'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Owoce');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Owoce</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Owoce'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Potrawy');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Potrawy</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Potrawy'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Przekąski');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Przekąski</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Przekąski'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Przetwory');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Przetwory</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Przetwory'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Ryby i owoce morza');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Ryby i owoce morza</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Ryby i owoce morza'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Słodycze, ciasta i desery');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Słodycze, ciasta i desery</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Słodycze, ciasta i desery'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Tłuszcze');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Tłuszcze</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Tłuszcze'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Warzywa');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Warzywa</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Warzywa'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Zboża');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Zboża</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Zboża'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
+                <?php
+                $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Zioła i przyprawy');
+                if(!empty($tablica['zalecane']) || !empty($tablica['dozwolony_w_umiarkowanych_ilościach']) || !empty($tablica['niewskazany'])){
+                ?>
                 <tr>
                     <td>Zioła i przyprawy</td>
-                    <td><?php $tablica = $main->print_produkty($_GET['wyswietl_diete'], 'Zioła i przyprawy'); echo $tablica['zalecane'];?></td>
+                    <td><?php echo $tablica['zalecane'];?></td>
                     <td><?php echo $tablica['dozwolony_w_umiarkowanych_ilościach'];?></td>
                     <td><?php echo $tablica['niewskazany'];?></td>
                 </tr>
+                <?php } ?>
         </tbody>
     </table>
 </div>
